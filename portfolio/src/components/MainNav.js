b590a64fa0ad384fac9abbe621cb3b22d3863756
Cameron Bisca <cjbisca@gmail.com>
2021-07-04 02:07:45 -0400

    Font family for showcase name and section headers + text-shadow

--- portfolio/src/components/MainNav.js
+++ portfolio/src/components/MainNav.js
@@ -55,7 +55,7 @@ function MainNav(){
             <section id="showcase">
                 <div id="showcase__content">
                     <h1 class="showcase__content--title"> Cameron Bisca </h1>
-                    <p class="showcase__content--para"> Full Stack Developer </p>
+                    <p class="showcase__content--para"> Front End Developer </p>
                     <a href="#" class="showcase__content--link"> My Work </a>
                 </div>
 
@@ -69,69 +69,79 @@ function MainNav(){
                 <div class="about-me__content">
                     <h2 class="about-me__content--title"> About Me </h2>
                     <p class="about-me__content--para"> 
+                        Successful people know that true education is what you get for yourself and by yourself, it’s not what someone gives or tells you, they know that true success in life does not come by luck, a lucky man is not a successful man, true winners know that they must develop skills and acquire knowledge before becoming truly successful.
+                        <br/>
+                        <br/>
                         Successful people know that true education is what you get for yourself and by yourself, it’s not what someone gives or tells you, they know that true success in life does not come by luck, a lucky man is not a successful man, true winners know that they must develop skills and acquire knowledge before becoming truly successful. 
                     </p>
-                    <a class="about-me__content--link"> Read More </a>
                 </div>
             </section>
 
-            <div class="projects-header-box">
+            {/* <div class="projects-header-box">
                 <h2 class="projects-header"> Projects </h2>
-            </div>
+            </div> */}
 
             <section id="projects">
-                <div class="project__card">
-                    <img src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/03/07/11/brooklyn-manhattan-bridge.jpg?width=1200&auto=webp&quality=75" alt="poster" />
-                </div>
+                <h1 class="projects-header"> Projects </h1>
+                <div class="project-list">
+                    <div class="project__card">
+                        <img src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/03/07/11/brooklyn-manhattan-bridge.jpg?width=1200&auto=webp&quality=75" alt="poster" />
+                    </div>
 
-                <div class="project__card">
-                    <img src="https://travelforlifenow.com/travelforlifenow.com/uploads/2019/07/Jefferson-St-Bushwick-June-2019-tny.jpg" alt="poster" />
-                </div>
+                    <div class="project__card">
+                        <img src="https://travelforlifenow.com/travelforlifenow.com/uploads/2019/07/Jefferson-St-Bushwick-June-2019-tny.jpg" alt="poster" />
+                    </div>
 
-                <div class="project__card">
-                    <img src="https://patch.com/img/cdn20/users/22926829/20170623/095152/styles/raw/public/article_images/bed_stuy_kathleen_culliton-1498225756-7379.jpg" alt="poster" />
-                </div>
+                    <div class="project__card">
+                        <img src="https://patch.com/img/cdn20/users/22926829/20170623/095152/styles/raw/public/article_images/bed_stuy_kathleen_culliton-1498225756-7379.jpg" alt="poster" />
+                    </div>
 
-                <div class="project__card">
-                    <img src="https://patch.com/img/cdn20/users/22926829/20170623/095152/styles/raw/public/article_images/bed_stuy_kathleen_culliton-1498225756-7379.jpg" alt="poster" />
-                </div>
+                    <div class="project__card">
+                        <img src="https://patch.com/img/cdn20/users/22926829/20170623/095152/styles/raw/public/article_images/bed_stuy_kathleen_culliton-1498225756-7379.jpg" alt="poster" />
+                    </div>
 
-                <div class="project__card">
-                    <img src="https://patch.com/img/cdn20/users/22926829/20170623/095152/styles/raw/public/article_images/bed_stuy_kathleen_culliton-1498225756-7379.jpg" alt="poster" />
-                </div>
+                    <div class="project__card">
+                        <img src="https://patch.com/img/cdn20/users/22926829/20170623/095152/styles/raw/public/article_images/bed_stuy_kathleen_culliton-1498225756-7379.jpg" alt="poster" />
+                    </div>
 
-                <div class="project__card">
-                    <img src="https://patch.com/img/cdn20/users/22926829/20170623/095152/styles/raw/public/article_images/bed_stuy_kathleen_culliton-1498225756-7379.jpg" alt="poster" />
+                    <div class="project__card">
+                        <img src="https://patch.com/img/cdn20/users/22926829/20170623/095152/styles/raw/public/article_images/bed_stuy_kathleen_culliton-1498225756-7379.jpg" alt="poster" />
+                    </div>
                 </div>
+                
             </section>
 
-            <div class="languages-header-box">
+            {/* <div class="languages-header-box">
                 <h2 class="languages-header"> Technologies </h2>
-            </div>
+            </div> */}
 
             <section id="languages">
-                <div class="lang-card lang-1">
-                    <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--jvDLhx0b--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/cpcr5w0kgl6j94tss7n9.png" alt="Ruby on Rails"/>
-                </div>
+                <h1 class="languages-header"> Technologies </h1>
+                
+                <div class="language-list">
+                    <div class="lang-card lang-1">
+                        <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--jvDLhx0b--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/cpcr5w0kgl6j94tss7n9.png" alt="Ruby on Rails"/>
+                    </div>
 
-                <div class="lang-card lang-2">
-                    <img src="https://sisnolabs.com/blog/wp-content/uploads/2020/09/reactjs.png" alt="React"/>
-                </div>
+                    <div class="lang-card lang-2">
+                        <img src="https://sisnolabs.com/blog/wp-content/uploads/2020/09/reactjs.png" alt="React"/>
+                    </div>
 
-                <div class="lang-card lang-3">
-                    <img src="https://techvccloud.mediacdn.vn/zoom/650_406/2018/11/23/js-15429579443112042672363-crop-1542957949936317424252.png" alt="JavaScript"/>
-                </div>
+                    <div class="lang-card lang-3">
+                        <img src="https://techvccloud.mediacdn.vn/zoom/650_406/2018/11/23/js-15429579443112042672363-crop-1542957949936317424252.png" alt="JavaScript"/>
+                    </div>
 
-                <div class="lang-card lang-4">
-                    <img src="https://pixelmechanics.com.sg/wp-content/uploads/2019/06/html5-logo-for-web-development.png" alt="HTML"/>
-                </div>
+                    <div class="lang-card lang-4">
+                        <img src="https://pixelmechanics.com.sg/wp-content/uploads/2019/06/html5-logo-for-web-development.png" alt="HTML"/>
+                    </div>
 
-                <div class="lang-card lang-5">
-                    <img src="https://allvectorlogo.com/img/2016/04/css3-logo.png" alt="CSS"/>
+                    <div class="lang-card lang-5">
+                        <img src="https://allvectorlogo.com/img/2016/04/css3-logo.png" alt="CSS"/>
+                    </div>
                 </div>
             </section>
             
-            <section id="testimonials">
+            {/* <section id="testimonials">
                 <div class="testimonial-card">
                     <div class="testimonial-card--img">
                         <img src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/03/07/11/brooklyn-manhattan-bridge.jpg?width=1200&auto=webp&quality=75" alt="I GOT NEXT" />
@@ -168,7 +178,7 @@ function MainNav(){
                         <p class="testimonial-card-content--para"> Demo Netlify Github </p>
                     </div>
                 </div>
-            </section>
+            </section> */}
 
             <section id="contact">
                 <form class="contact__form">
