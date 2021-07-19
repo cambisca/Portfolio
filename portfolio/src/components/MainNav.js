4b99dbc9df865cecad5f0d63db29afd53606b44b
Cameron Bisca <cjbisca@gmail.com>
2021-07-19 16:16:30 -0400

    1500px query for about-me, projects, technologies

--- portfolio/src/components/MainNav.js
+++ portfolio/src/components/MainNav.js
@@ -66,6 +66,7 @@ function MainNav(){
                 </nav>
             </header>
 
+            
             <section id="showcase">
                 <div id="showcase__content">
                     <h1 class="showcase__content--title"> Cameron Bisca </h1>
@@ -73,91 +74,95 @@ function MainNav(){
                 </div>
             </section>
 
-            <section id="about-me">
-                <div class="about-me__image-box">
-                    <img class="about-me__image" src='https://avatars.githubusercontent.com/u/69369814?s=400&u=d66d868b4818253cdc811b47ed9745219eb96709&v=4' alt="Cam" />
-                </div>
+            <section class="about-me-wrapper">
+                <section id="about-me">
+                    <div class="about-me__image-box">
+                        <img class="about-me__image" src='https://avatars.githubusercontent.com/u/69369814?s=400&u=d66d868b4818253cdc811b47ed9745219eb96709&v=4' alt="Cam" />
+                    </div>
 
-                <div class="about-me__content">
-                    <h2 class="about-me__content--title"> About Me </h2>
-                    <p class="about-me__content--para"> 
-                        Yo! I'm Cameron but I usually go by Cam. I am a Brooklyn boy and software engineer with a passion for expressive front end development. 
-                        
-                        <br/>
-                        <br/>
-                        
-                        Before discovering my love for development I studied acting at various conservatories. It was at those institutions where I became fascinated by relationships and the motives/objectives of individuals. 
+                    <div class="about-me__content">
+                        <h2 class="about-me__content--title"> About Me </h2>
+                        <p class="about-me__content--para"> 
+                            Yo! I'm Cameron but I usually go by Cam. I am a Brooklyn boy and software engineer with a passion for expressive front end development. 
+                            
+                            <br/>
+                            <br/>
+                            
+                            Before discovering my love for development I studied acting at various conservatories. It was at those institutions where I became fascinated by relationships and the motives/objectives of individuals. 
 
-                        <br/>
-                        <br/>
+                            <br/>
+                            <br/>
 
-                        This kind of education led me to where I am present day. Today, I aim to build applications that are not only useful but expressive and in adjustment. 
+                            This kind of education led me to where I am present day. Today, I aim to build applications that are not only useful but expressive and in adjustment. 
 
-                        <br/>
-                        <br/>
+                            <br/>
+                            <br/>
 
-                        When I've logged off you can find me on a basketball court, in a theatre, music venue, cocktail bar or hiking trail :) 
-                    </p>
-                </div>
+                            When I've logged off you can find me on a basketball court, in a theatre, music venue, cocktail bar or hiking trail :) 
+                        </p>
+                    </div>
+                </section>
+            </section>
+            <section class="projects-container">
+                <section id="projects">
+                    <h1 class="projects-header"> Projects </h1>
+                    <div class="project-list">
+                        <div class="project__card">
+                            <img src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/03/07/11/brooklyn-manhattan-bridge.jpg?width=1200&auto=webp&quality=75" alt="poster" />
+                        </div>
+
+                        <div class="project__card">
+                            <img src="https://travelforlifenow.com/travelforlifenow.com/uploads/2019/07/Jefferson-St-Bushwick-June-2019-tny.jpg" alt="poster" />
+                        </div>
+
+                        <div class="project__card">
+                            <img src="https://patch.com/img/cdn20/users/22926829/20170623/095152/styles/raw/public/article_images/bed_stuy_kathleen_culliton-1498225756-7379.jpg" alt="poster" />
+                        </div>
+
+                        <div class="project__card">
+                            <img src="https://patch.com/img/cdn20/users/22926829/20170623/095152/styles/raw/public/article_images/bed_stuy_kathleen_culliton-1498225756-7379.jpg" alt="poster" />
+                        </div>
+
+                        <div class="project__card">
+                            <img src="https://patch.com/img/cdn20/users/22926829/20170623/095152/styles/raw/public/article_images/bed_stuy_kathleen_culliton-1498225756-7379.jpg" alt="poster" />
+                        </div>
+
+                        <div class="project__card">
+                            <img src="https://patch.com/img/cdn20/users/22926829/20170623/095152/styles/raw/public/article_images/bed_stuy_kathleen_culliton-1498225756-7379.jpg" alt="poster" />
+                        </div>
+                    </div>
+                    
+                </section>
             </section>
 
-            <section id="projects">
-                <h1 class="projects-header"> Projects </h1>
-                <div class="project-list">
-                    <div class="project__card">
-                        <img src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/03/07/11/brooklyn-manhattan-bridge.jpg?width=1200&auto=webp&quality=75" alt="poster" />
-                    </div>
+            <section id="language-container">
+                <section id="languages">
+                    <h1 class="languages-header"> Technologies </h1>
 
-                    <div class="project__card">
-                        <img src="https://travelforlifenow.com/travelforlifenow.com/uploads/2019/07/Jefferson-St-Bushwick-June-2019-tny.jpg" alt="poster" />
-                    </div>
+                    <div class="language-list">
+                        <div class="lang-card lang-1">
+                            <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--jvDLhx0b--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/cpcr5w0kgl6j94tss7n9.png" alt="Ruby on Rails"/>
+                        </div>
 
-                    <div class="project__card">
-                        <img src="https://patch.com/img/cdn20/users/22926829/20170623/095152/styles/raw/public/article_images/bed_stuy_kathleen_culliton-1498225756-7379.jpg" alt="poster" />
-                    </div>
+                        <div class="lang-card lang-2">
+                            <img src="https://sisnolabs.com/blog/wp-content/uploads/2020/09/reactjs.png" alt="React"/>
+                        </div>
 
-                    <div class="project__card">
-                        <img src="https://patch.com/img/cdn20/users/22926829/20170623/095152/styles/raw/public/article_images/bed_stuy_kathleen_culliton-1498225756-7379.jpg" alt="poster" />
-                    </div>
+                        <div class="lang-card lang-3">
+                            <img src="https://techvccloud.mediacdn.vn/zoom/650_406/2018/11/23/js-15429579443112042672363-crop-1542957949936317424252.png" alt="JavaScript"/>
+                        </div>
 
-                    <div class="project__card">
-                        <img src="https://patch.com/img/cdn20/users/22926829/20170623/095152/styles/raw/public/article_images/bed_stuy_kathleen_culliton-1498225756-7379.jpg" alt="poster" />
-                    </div>
+                        <div class="lang-card lang-4">
+                            <img src="https://pixelmechanics.com.sg/wp-content/uploads/2019/06/html5-logo-for-web-development.png" alt="HTML"/>
+                        </div>
 
-                    <div class="project__card">
-                        <img src="https://patch.com/img/cdn20/users/22926829/20170623/095152/styles/raw/public/article_images/bed_stuy_kathleen_culliton-1498225756-7379.jpg" alt="poster" />
+                        <div class="lang-card lang-5">
+                            <img src="https://allvectorlogo.com/img/2016/04/css3-logo.png" alt="CSS"/>
+                        </div>
                     </div>
-                </div>
-                
+                </section>
             </section>
 
-            <section id="languages">
-                <h1 class="languages-header"> Technologies </h1>
-
-                <div class="language-list">
-                    <div class="lang-card lang-1">
-                        <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--jvDLhx0b--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/cpcr5w0kgl6j94tss7n9.png" alt="Ruby on Rails"/>
-                    </div>
-
-                    <div class="lang-card lang-2">
-                        <img src="https://sisnolabs.com/blog/wp-content/uploads/2020/09/reactjs.png" alt="React"/>
-                    </div>
-
-                    <div class="lang-card lang-3">
-                        <img src="https://techvccloud.mediacdn.vn/zoom/650_406/2018/11/23/js-15429579443112042672363-crop-1542957949936317424252.png" alt="JavaScript"/>
-                    </div>
-
-                    <div class="lang-card lang-4">
-                        <img src="https://pixelmechanics.com.sg/wp-content/uploads/2019/06/html5-logo-for-web-development.png" alt="HTML"/>
-                    </div>
-
-                    <div class="lang-card lang-5">
-                        <img src="https://allvectorlogo.com/img/2016/04/css3-logo.png" alt="CSS"/>
-                    </div>
-                </div>
-            </section>
-            
-            
 
             <section id="contact">
                 <form class="contact__form" >
